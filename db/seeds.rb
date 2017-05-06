2cbbf5a340bb60766910dbd7d53753511a06ec03
Megan Wilkin <megan.wilkin@blueostrichdesign.co.uk>
2017-05-06 15:05:12 +0100

    authorisations and link restrictions added

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,11 +38,20 @@ users = User.all
    )
  end
 
- user = User.first
- user.update_attributes!(
-   email: 'megan@megan-and-andy.co.uk',
-   password: 'helloworld'
- )
+ # Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
 
  puts "Seed finished"
  puts "#{User.count} users created"
